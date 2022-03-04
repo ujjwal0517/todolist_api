0a82e63828645a6193d5f9ae7ef55c2879fbb2c4
ujjwal0517 <rupasahu1999@gmail.com>
2022-03-04 22:17:33 +0545

    Final commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -33,4 +33,29 @@ exports.createTodo = async(req, res)=>{
   }
 
 }
+exports.updateTodo = async(req, res)=>{
+    const id = req.params.id;
+    const updatedTodo = await Todo.findById(id); 
+    if( updatedTodo!==null){
+    updatedTodo.todo = req.body.todo;
+    updatedTodo.active = req.body.active;
+    const newUpdatedTodo = await updatedTodo.save();
+    res.json(newUpdatedTodo)
+    }
+    else{
+        res.status(404).json({"message":"todo not found"})
+    }
+}
+exports.deleteTodo = async(req,res)=>{
+    try{
+    const id = req.params.id;
+    const todo = Todo.findById(id);
+    
+    await todo.remove();
+    res.status(200).json({"message": "Deleted Successfully"}) }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
+
+}
 
--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const {getTodo,getTodoById,  createTodo} = require('../controllers/todoController.js');
+const { getTodo, getTodoById,  createTodo , updateTodo, deleteTodo} = require('../controllers/todoController.js');
 
 //getting all todos from database
 router.get('/', getTodo);
 router.get('/:id', getTodoById);
 router.post('/', createTodo);
+router.put('/:id', updateTodo)
+router.delete('/:id', deleteTodo)
+
 
 module.exports = router;
